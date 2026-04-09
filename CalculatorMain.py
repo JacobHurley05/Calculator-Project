000f655446361b93a117d4cca1103aa1d536abd7
Jacob <jacobh@jacobah.com>
2026-04-09 09:31:53 -0400

    Added my first window using tkinter.

---
+++ CalculatorMain.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk() #create a window
+window.title("My Calculator") #title of the window
+window.geometry("400x600") #size of the window
+
+
+
+#execute the window
+window.mainloop() #show the window
